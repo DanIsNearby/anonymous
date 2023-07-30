65e57b01087a05d0bfeee267178ea80a65556a01
DanIsNearby.eth <91475837+DanIsNearby@users.noreply.github.com>
2023-07-30 17:52:46 +0200

    testing user membership

--- bot.js
+++ bot.js
@@ -5,7 +5,24 @@ const bot = new Telegraf(BOT_TOKEN);
 
 bot.on('text', async ctx => {
     if (ctx.message.chat.type === 'private') {
-        console.log(ctx.message.chat);
+        const content = ctx.message.chat.text
+        const userId = ctx.message.from.id
+        const chatId = `-1677675912`
+        const chatId2 = `-1677675912`
+        bot.telegram.getChatMember(chatId, userId).then((chatMember) => {
+            if (chatMember && chatMember.status === `member`){
+                console.log(`User is a member of that group chat #1`)
+            }else{
+                console.log(`User is not a member of that group chat #1`) 
+            }
+        })
+        bot.telegram.getChatMember(chatId2, userId).then((chatMember) => {
+            if (chatMember && chatMember.status === `member`){
+                console.log(`User is a member of that group chat #2`)
+            }else{
+                console.log(`User is not a member of that group chat #2`)
+            }
+        })
     }
 })
 
