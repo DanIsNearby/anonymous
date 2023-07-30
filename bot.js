6ec1435f5d43cc045fe980ba209163a57123627f
DanIsNearby.eth <91475837+DanIsNearby@users.noreply.github.com>
2023-07-30 17:27:46 +0200

    Message info test

--- bot.js
+++ bot.js
@@ -1,1 +1,12 @@
+require('dotenv').config()
+const { Telegraf } = require('telegraf')
+const BOT_TOKEN = process.env.BOT_TOKEN
+const bot = new Telegraf(BOT_TOKEN)
 
+bot.on('text', async ctx => {
+    if (ctx.message.chat.type === 'private') {
+        console.log(ctx.message.chat)
+    }
+}
+
+bot.launch()
