8b47589813a6c49e7572e9810879fedde71a7ff4
DanIsNearby.eth <91475837+DanIsNearby@users.noreply.github.com>
2023-07-30 17:59:37 +0200

    chat test

--- bot.js
+++ bot.js
@@ -7,8 +7,8 @@ bot.on('text', async ctx => {
     if (ctx.message.chat.type === 'private') {
         const content = ctx.message.chat.text
         const userId = ctx.message.from.id
-        const chatId = -1677675912
-        const chatId2 = -841365986
+        const userId2 = 5748601508
+        const chatId = 6443812256
         bot.telegram.getChatMember(chatId, userId).then((chatMember) => {
             if (chatMember && chatMember.status === `member`){
                 console.log(`User is a member of that group chat #1`)
@@ -16,7 +16,7 @@ bot.on('text', async ctx => {
                 console.log(`User is not a member of that group chat #1`) 
             }
         })
-        bot.telegram.getChatMember(chatId2, userId).then((chatMember) => {
+        bot.telegram.getChatMember(chatId, userId2).then((chatMember) => {
             if (chatMember && chatMember.status === `member`){
                 console.log(`User is a member of that group chat #2`)
             }else{
