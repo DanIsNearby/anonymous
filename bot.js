78b81ac567b400fb735f1f0abb1359e9a6fc1c57
DanIsNearby.eth <91475837+DanIsNearby@users.noreply.github.com>
2023-07-30 18:09:16 +0200

    Fixing "user not found error"

--- bot.js
+++ bot.js
@@ -7,15 +7,16 @@ bot.on('text', async ctx => {
     if (ctx.message.chat.type === 'private') {
         const content = ctx.message.chat.text
         const userId = ctx.message.from.id
-        console.log(userId)
         const userId2 = 5748601508
         const chatId = 6443812256
-        bot.telegram.getChatMember(chatId, userId).then((chatMember) => {
-            if (chatMember && chatMember.status === `member`){
-                console.log(`User is a member of that group chat #1`)
-            }else{
-                console.log(`User is not a member of that group chat #1`) 
-            }
+        bot.telegram.getChatMembersCount(chatId).then((count) => {
+            bot.telegram.getChatMember(chatId, userId).then((chatMember) => {
+                if (chatMember && chatMember.status === `member`){
+                    console.log(`User is a member of that group chat #1`)
+                }else{
+                    console.log(`User is not a member of that group chat #1`) 
+                }
+            })
         })
         bot.telegram.getChatMember(chatId, userId2).then((chatMember) => {
             if (chatMember && chatMember.status === `member`){
