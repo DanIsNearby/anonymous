f7595c571f33c61f70c42aae87390e732ba170e0
DanIsNearby.eth <91475837+DanIsNearby@users.noreply.github.com>
2023-07-30 17:29:22 +0200

    syntax fix

--- bot.js
+++ bot.js
@@ -1,12 +1,12 @@
-require('dotenv').config()
-const { Telegraf } = require('telegraf')
-const BOT_TOKEN = process.env.BOT_TOKEN
-const bot = new Telegraf(BOT_TOKEN)
+require('dotenv').config();
+const { Telegraf } = require('telegraf');
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const bot = new Telegraf(BOT_TOKEN);
 
 bot.on('text', async ctx => {
     if (ctx.message.chat.type === 'private') {
-        console.log(ctx.message.chat)
+        console.log(ctx.message.chat);
     }
-}
+})
 
-bot.launch()
+bot.launch();
